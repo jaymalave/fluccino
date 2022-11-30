d33bbf3d3e3cb3476d22cf4a6d8aad91e062bd67
jaymalave <jaym.github@gmail.com>
2022-12-01 01:19:28 +0530

    feat: setup auth config

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,35 @@
+import 'package:coffe/firebase_options.dart';
 import 'package:coffe/pages/firstpage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    @override
+    void initState() {
+      super.initState();
+      // Firebase.initializeApp().whenComplete(() {
+      //   print("completed");
+      //   setState(() {});
+      // });
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
